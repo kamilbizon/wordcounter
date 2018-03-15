db435273522a67de50123ed4d321ef2f6c9242ae
kamilbizon <kamilbizon@outlook.com>
2018-03-16 00:09:32 +0100

    flags

--- wc.py
+++ wc.py
@@ -3,23 +3,55 @@ from sys import argv, exit
 
 
 def main():
-    if len(argv) == 1:
-        pass
+
+    flags = []
+    if len(argv) == 2:
+        flags += ["-c", "-l", "-w"]
     elif len(argv) > 2:
-        if argv
+        if argv[2]:
+            for i in argv[1:-1]:
+                flags += [i]
 
     file = open_file()
 
     read_file = file.read()
 
-    number_of_lines = len(open(argv[-1], 'r').readlines())
-    number_of_words = len(read_file.split())
-    number_of_characters = len(read_file)
-    size_of_file = read_file.__sizeof__()
+    count_by_flags(flags, read_file)
+
 
     file.close()
 
-    print(number_of_lines, " ", number_of_words, " ", number_of_characters, " ", size_of_file, " ", argv[-1])
+
+
+def count_by_flags(flags, read_file):
+
+    if "--version" in flags:
+        print("First version of word counter copy by Kamil Bizoń :)")
+        exit()
+
+    if "-l" in flags or "--lines" in flags:
+        number_of_lines = len(open(argv[-1], 'r').readlines())
+        print(number_of_lines, " ", end='')
+
+    if "-w" in flags or "-words" in flags:
+        number_of_words = len(read_file.split())
+        print(number_of_words, " ", end='')
+
+    if "-m" in flags or "--chars" in flags:
+        number_of_characters = len(read_file)
+        print(number_of_characters, " ", end='')
+
+    if "-c" in flags or "--bytes" in flags:
+        size_of_file = int(read_file.__sizeof__() / 4)
+        print(size_of_file, " ", end='')
+
+    if "-L" in flags or "--max-line-length" in flags:
+        longest_line_length = 15
+        print(longest_line_length)
+
+    print(argv[-1])
+
+
 
 
 def open_file():
